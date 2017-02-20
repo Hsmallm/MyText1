e5c50a409a6fef8487086d8c258f8081611d0420
hzhm <1828609633@>
2017-02-20 18:55:53 +0800

    ConstraintLayout约束布局...

---
+++ app/src/main/java/com/example/administrator/text1/ui/testAndroid/testOther/TestConstraintLayout.java
@@ -0,0 +1,19 @@
+package com.example.administrator.text1.ui.testAndroid.testOther;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.text1.R;
+
+/**
+ * Created by hzhm on 2017/2/20.
+ */
+
+public class TestConstraintLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_constraintlayout);
+    }
+}
